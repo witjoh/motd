c46fe8eb1e7a99d403cb7581e0a620502f505dd4
Johan De Wit <johan@open-future.be>
2014-06-14 17:56:57 +0200

    rspec testing is working

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ motd
 ====
 This is the motd module. (based on the puppetlabs/motd module)
 
+Added all rspec and provided full code coverage
+
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,8 +1,37 @@
 require 'spec_helper'
 describe 'motd' do
 
-  context 'with defaults for all parameters' do
+  context 'on a linux system' do
+    let(:facts) {{ :kernel                 => 'linux',
+                  :hostname                => 'testhost',
+                  :domain                  => 'somedomain',
+                  :puppetversion           => '3.3.3',
+                  :environment             => 'testenv',
+                  :operatingsystem         => 'someos',
+                  :operatingsystemrelease => 'somerelease',
+                  :architecture            => 'manybits', }}
+
     it { should contain_class('motd') }
+    it do
+      should contain_file('/etc/motd').with(
+        { 'ensure'  => 'file', }
+      )
+    end
+    ['testhost','somedomain','3.3.3','testenv','someos','somerelease','manybits'].each { |important|
+      it do
+        should contain_file('/etc/motd').with(
+          { 'content' => /#{important}/, },
+        )
+      end
+    }
   end
+
+  context "not on a linux system" do
+    let(:facts) {{ :kernel => 'exoticOS', }}
+
+    it { should contain_class('motd') }
+    it { should_not contain_file('/etc/motd') }
+  end
+
 end
 
